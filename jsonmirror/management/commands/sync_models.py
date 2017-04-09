3a1896aa68865cd539a03cfb6e57b37a8a9cdd95
Win <synwe@yahoo.com>
2017-04-09 15:15:39 +0000

    Fix sync_models command + cleanup

--- jsonmirror/management/commands/sync_models.py
+++ jsonmirror/management/commands/sync_models.py
@@ -1,36 +1,32 @@
 from __future__ import print_function
 from django.core.management.base import BaseCommand
-from django.conf import settings
 from jsonmirror.management.conf import bcolors
-from jsonmirror.conf import BACKEND
-from jsonmirror.utils import get_model_from_conf, mirror_model
+from jsonmirror.producers import mirror_model_s
+from jsonmirror.conf import get_model_from_path, MIRRORED_MODELS
 
 
 class Command(BaseCommand):
-    help = 'Mirror models into '+BACKEND
+    help = 'Mirror models whith jsonmirror'
 
     def handle(self, *args, **options):
-        if BACKEND == "rethinkdb":
-            num_instances = 0
-            num_models = 0
-            mirrored_models = getattr(settings, 'MIRRORED_MODELS', [])
-            for modconf in mirrored_models:
-                model, options = get_model_from_conf(modconf)
-                modelname = str(model._meta)
-                print("* Syncing model "+bcolors.OKBLUE+modelname+bcolors.ENDC)
-                instances = model.objects.all()
-                created = 0
-                updated = 0
-                for instance in instances:
-                    res = mirror_model(instance)
-                    if res["created"] == 1:
-                        created += 1
-                    if res["updated"] == 1:
-                        updated += 1
-                    num_instances += 1
-                num_models += 1
-                print(str(created)+" documents created")
-                print(str(updated)+" documents updated")
-        print("[ "+bcolors.OKGREEN+bcolors.BOLD+"OK"+bcolors.ENDC+" ] "+bcolors.BOLD+str(num_instances)+bcolors.ENDC+" instances synced in "+\
-        str(num_models)+" models")
+        num_models = 0
+        num_instances = 0
+        for modelpath in MIRRORED_MODELS.keys():
+            num_models += 1
+            model = get_model_from_path(modelpath)
+            print("* Syncing model "+bcolors.OKBLUE+modelpath+bcolors.ENDC)
+            created = 0
+            updated = 0
+            for instance in model.objects.all():
+                num_instances += 1
+                c, u = mirror_model_s(instance)
+                if c is True:
+                    created += 1
+                if u is True:
+                    updated += 1
+            print(bcolors.BOLD+bcolors.BOLD+str(created)+bcolors.ENDC+" documents created")
+            print(bcolors.BOLD+bcolors.BOLD+str(created)+bcolors.ENDC+" documents updated")
+        print("[ "+bcolors.OKGREEN+bcolors.BOLD+"OK"+bcolors.ENDC+" ] "+bcolors.BOLD+\
+              str(num_instances)+bcolors.ENDC+" instances synced in "+\
+              str(num_models)+" models")
         return
--- jsonmirror/utils.py
+++ jsonmirror/utils.py
@@ -2,26 +2,7 @@ from __future__ import print_function
 import json
 from django.core import serializers
 from django.utils import timezone
-from django.conf import settings
-from jsonmirror.conf import BACKEND, TABLE, get_option
-if BACKEND == "rethinkdb":
-    from jsonmirror.backends.rethinkdb import mirror_model, delete_model
-elif BACKEND == "sqlite":
-    from jsonmirror.backends.sqlite import mirror_model, delete_model
 
-
-def delete_model_s(instance):
-    delete_model(instance)
-    return
-
-def mirror_model_s(instance, created=False, verbose=False):
-    table = TABLE
-    custom_table = get_option(instance, "table")
-    if custom_table is not None:
-        table = custom_table
-    data = prepare_data(instance)
-    res = mirror_model(instance, data, created, verbose, table)
-    return res
 
 def prepare_data(instance):
     # prepare data
